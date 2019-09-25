70612ffc763c5c66dd5721e2e65fa05a90ffeedb
Prestamo Estudiantes <prestamour@urosario.edu.co>
2019-09-25 17:02:14 -0500

    Archivos ejemplo 2 a subir

---
+++ script1.js
@@ -0,0 +1,17 @@
+//console.log("hola mundo desde el archivo");
+
+//var x = -15;
+//x >= 0? console.log(x) : console.log(-x);
+
+// {
+//     x = Math.PI;
+//     cx = Math.cos(x);
+//     console.log('cs=' ,cx)
+// }
+
+function valorabsoluto(x){
+    return x < 0 ? console.log(-x)
+     : console.log(x)
+}
+
+valorabsoluto(-5)
